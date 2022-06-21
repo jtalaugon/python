49cffabd08d9a4b2ebce6b1c50582013652d532f
John Talaugon <jtalaugo@asu.edu>
2022-06-20 22:19:16 -0700

    First lesson

---
+++ introduction/simple_variable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+var_str = "this is a string"
+var_int = 49
+var_float = 39.01
+
+print("var_str is of type %s" % (type(var_str)))
+print("var_int is of type %s" % (type(var_int)))
+print("var_float is of type %s" % (type(var_float)))
+
